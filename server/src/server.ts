cbdb1ab85d9577e2fc86fe8896f85aa8bcf0429e
Heliton Oliveira <heliton.oliveira88@gmail.com>
2020-06-02 19:49:25 -0300

    back-end finalized

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  response.json([
-    'Heliton',
-    'Diego',
-    'Paula'
-  ]);
-});
+app.use(cors())
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
